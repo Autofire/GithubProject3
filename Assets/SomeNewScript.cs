d806f98b6f2fb1e95b68e6f7e26f9c83519e8816
Autofire <Autofire@users.noreply.github.com>
2020-10-09 16:27:42 -0700

    Now we're saying something about today.

--- Assets/SomeNewScript.cs
+++ Assets/SomeNewScript.cs
@@ -8,6 +8,7 @@ public class SomeNewScript : MonoBehaviour
     void Start()
     {
 		Debug.Log("Hello world");
+		Debug.Log("Today is a very good day for staying inside");
     }
 
     // Update is called once per frame
